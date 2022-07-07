c6519ae532d106d6584f4d7fbb0a18457ec1b58f
RushankWani <97100007+RushankWani@users.noreply.github.com>
2022-07-07 19:17:26 +0530

    Time: 74 ms (16.41%), Space: 14 MB (32.25%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -1,26 +1,24 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        result = ListNode()
-        head = result
-
-        while list1 and list2:
-            if list1.val <= list2.val:
-                head.next = list1
-                list1 = list1.next
-            else:
-                head.next = list2
-                list2 = list2.next
-            head = head.next
-
-        if list1:
-            head.next = list1
-        elif list2:
-            head.next = list2
-
-        return result.next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1=list1
+        curr2=list2
+        curr3=ListNode()
+        head=curr3
         
+        while curr1 and curr2:
+            if curr1.val<curr2.val:
+                head.next=curr1
+                curr1,head=curr1.next,curr1
+            else:
+                head.next=curr2
+                curr2,head=curr2.next,curr2
+        
+        if curr1 or curr2:
+            head.next=curr1 if curr1 else curr2
+        
+        return curr3.next
